68337c830fd63842917b87014363f90a9d89990c
John <mrjohnishappy@gmail.com>
2023-04-13 00:20:44 -0400

    Added card preview on hover!! :)

---
+++ js/preview.js
@@ -0,0 +1,34 @@
+var hoverCardName;
+
+$(document).on('mouseenter', '.card-zone-square, .card',
+
+    // Mouse enter
+    function() {
+
+        if (hoverCardName || !$(this).attr('data-card-name')) { return; } // Don't do anything if card already selected
+
+        hoverCardName = $(this).attr('data-card-name')
+        console.log(hoverCardName)
+
+        const cardImgURL = 'cards/' + cards[hoverCardName]['file']
+        console.log(cardImgURL)
+
+        //$('#preview-card').css('background-image', 'url(' + cardImgURL + ')')
+        $('#preview-card-img').attr('src', cardImgURL)
+
+        console.log('Set preview-card to ' + cardImgURL)
+
+    }
+
+)
+
+$(document).on('mouseleave', '.card-zone-square, .card', 
+
+    // Mouse leave
+    function() {
+
+        hoverCardName = null;
+        $('#preview-card-img').attr('src', null)
+
+    }
+)
